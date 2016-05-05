e3fa00175060b77aa48be130755a180c73623394
Alexandr <Elizarov>
2016-05-05 11:37:24 +0300

    общие для ассемблера и симулятора константы хранятся здесь

---
+++ CPUSimulator/myarchitecture.h
@@ -0,0 +1,13 @@
+#ifndef myarch
+#define myarch
+
+/*connect with my arhitecture, so they are important both
+for assembly and simulator
+*/
+int const OPCODE_LENGTH = 5;
+int const REGCODE_LENGTH = 5;
+int const IMM = 16;
+int const REG_COUNT = 19;
+int const CAPACITY = 32;
+
+#endif
