dbfb0f685b55c9e0aa18acc1a40955165f54db07
Alexandr <Elizarov>
2016-05-05 13:15:20 +0300

    Ведем диалог с пользователем. Все готова к передаче программ операционной системе

--- CPUSimulator/MyComputer.cpp
+++ CPUSimulator/MyComputer.cpp
@@ -4,24 +4,21 @@
 #include <string>
 #include "Assembly.h"
 #include "comp.h"
-//#include "CPUSimulatorHeader.h"
+#include "Utils.h"
 
 using namespace std;
 
 int main() {
-	cout << "Do you want to compile or execute programm? (c/e)";
-	char mode;
-	cin >> mode;
-	while (mode != 'c' && mode != 'e' && mode != 'C' && mode != 'E') {
-		cout << "Please enter c or e (compile or execute)";
-		cin >> mode;
-	}
-	cin.get();
+	cout << "Do you want to compile or execute programm? (c/e): ";
+	char mode = choiceMode();
 	if (mode == 'c' || mode == 'C') {
-		compile(inputProgramForCompile());
+		complileMode();
 	}
 	if (mode == 'e' || mode == 'E') {
-		inputProgrammsForExecute();
+		executeMode();
+	}
+	if (mode == 'q') {
+		cout << "You don't choice any mode. Programm finishs. Good buy!" << endl;
 	}
 	_getch();
 }
@@ -32,11 +29,10 @@ string inputProgramForCompile() {
 	cin >> pathToAsmProgramm;
 	ifstream fileReader;
 	fileReader.open(pathToAsmProgramm);
-	while (!fileReader.is_open()) {
-		cout << "File with the specified name does not exist. Please enter a different name: ";
-		cin >> pathToAsmProgramm;
-		fileReader.open(pathToAsmProgramm);
-	}
+	string validateMsgs[3] = {"File with the specified name does not exist. ", 
+		"Wrong extension of file. Extensiom have to be .txt. ", 
+		"Please enter a different file name or q to quite: "};
+	validatePath(validateMsgs, fileReader, pathToAsmProgramm, "txt");
 	return pathToAsmProgramm;
 }
 
@@ -47,17 +43,76 @@ vector<string> inputProgrammsForExecute() {
 	while (true) {
 		cout << "Enter the full name of programm or q to quite: ";
 		getline(cin, path);
-		if (path == "q") {
+		if (path == "q" || path == "Q") {
 			break;
 		}
-		cout << path << endl;
 		fileReader.open(path);
-		while (!fileReader.is_open()) {
-			cout << "File with the specified name does not exist. Please enter a different name: ";
-			getline(cin, path);
-			fileReader.open(path);
+		string validateMsgs[3] = { "Programm with the specified name does not exist. ",
+			"Wrong extension of file.Extensiom have to be.dat. ",
+			"Please enter a different programm or q to quite: " };
+		validatePath(validateMsgs, fileReader, path, "tad");
+		if (path != "") {
+			programmNames.push_back(path);
 		}
-		programmNames.push_back(path);
 	}
 	return programmNames;
+}
+
+void validatePath(string* validateMsgs, ifstream& fileReader, string& path, string extension) {
+	while (!fileReader.is_open() || cutOffSubstrFromStr(reverse(path), '.') != extension) {
+		if (!fileReader.is_open()) {
+			cout << validateMsgs[0];
+		}
+		else if (cutOffSubstrFromStr(reverse(path), '.') != extension) {
+			cout << validateMsgs[1];
+		}
+		cout << validateMsgs[2];
+		cin >> path;
+		if (path == "q" || path == "Q") {
+			path = "";
+			break;
+		}
+		fileReader.open(path);
+		cin.get();
+	}
+}
+
+void complileMode() {
+	string programmForCompile = inputProgramForCompile();
+	if (programmForCompile == "") {
+		cout << "You don't choice any files. Programm finishs. Good buy!" << endl;
+	}
+	else {
+		compile(programmForCompile);
+	}
+}
+
+void executeMode() {
+	vector<string> programmsForExecute = inputProgrammsForExecute();
+	if (programmsForExecute.size() > 0) {
+
+		//debug
+		if (programmsForExecute.size() > 0) {
+			for (string item : programmsForExecute) {
+				cout << item << endl;
+			}
+		}
+	}
+	else {
+		cout << "You don't choice any programms for execute. Programm finishs. Good buy!" << endl;
+	}
+}
+
+char choiceMode() {
+	char mode;
+	cin >> mode;
+	while (mode != 'c' && mode != 'e' && mode != 'C' && mode != 'E') {
+		cout << "Please enter c or e (compile or execute) or q to quite: ";
+		cin >> mode;
+		if (mode == 'q' || mode == 'Q') {
+			return 'q';
+		}
+	}
+	cin.get();
+	return mode;
 }
